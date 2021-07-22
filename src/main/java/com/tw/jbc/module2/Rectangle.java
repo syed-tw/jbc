b459f33975c57d06f087365f75f66a2658ba5fa2
masroor.rizvi@gmail.com <masroor.rizvi@gmail.com>
2021-07-22 19:44:37 +0530

    added area to the rectangle

--- src/main/java/com/tw/jbc/module2/Rectangle.java
+++ src/main/java/com/tw/jbc/module2/Rectangle.java
@@ -1,4 +1,8 @@
 package com.tw.jbc.module2;
 
 public class Rectangle {
+    public int calculateArea(int length, int breath) {
+        int area = length*breath;
+        return area;
+    }
 }
--- src/test/java/com/tw/jbc/module2/RectangleTest.java
+++ src/test/java/com/tw/jbc/module2/RectangleTest.java
@@ -2,11 +2,15 @@ package com.tw.jbc.module2;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class RectangleTest {
     @Test
-    void checkAreaOfRectangle() {
+    void testCalculateArea() {
         Rectangle rectangle = new Rectangle();
+        int area = rectangle.calculateArea(3,4);
+        assertEquals(12, area);
+
     }
 }
