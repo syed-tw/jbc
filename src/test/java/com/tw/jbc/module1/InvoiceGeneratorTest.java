3203c5bfe92e85c16c749e42896076848776d096
syed <masroor.rizvi@gmail.com>
2021-07-25 12:48:43 +0530

    Cab Invoice - Requirement 1 Commit

--- src/test/java/com/tw/jbc/module1/InvoiceGeneratorTest.java
+++ src/test/java/com/tw/jbc/module1/InvoiceGeneratorTest.java
@@ -4,20 +4,28 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+
 public class InvoiceGeneratorTest {
+
+    /***
+     * Requirement 1 - Test Case
+     */
     @Test
     void checkFareWhenTotalFareIsLessThanMinimumFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         int fare = invoiceGenerator.getFare(3,2);
-        assertEquals(40, fare);
+        assertEquals(34, fare);
     }
 
-
-    @Test
-    void checkFareWhenTotalFareIsGreaterThanMinimumFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        int fare = invoiceGenerator.getFare(4,4);
-        assertEquals(48, fare);
-    }
+    /***
+     * Requirement 2 - Test Case
+     */
+//    @Test
+//    void checkFareWhenTotalFareIsGreaterThanMinimumFare() {
+//        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+//        int fare = invoiceGenerator.getFare(4,4);
+//        assertEquals(48, fare);
+//    }
 
 }
